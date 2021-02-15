bf85a53ad318bcb0b40a82c74ee9d341cc8fb255
Wilton Ribeiro <wiltonribeiromaster@gmail.com>
2021-02-15 11:34:23 -0300

    reduzir o tempo entre as vibrações e setar timer em 1m

--- timer.sh
+++ timer.sh
@@ -5,7 +5,7 @@ echo "Programa iniciado!";
 vibrate(){
   for ((i = 0 ; i < $1 ; i++)); do
     termux-vibrate -fd 1500;
-    sleep 2;
+    sleep 1;
   done
 }
 
@@ -14,7 +14,7 @@ timer(){
   mesage="";
 
   # for debug
-  #minutes=$1
+  minutes=$1
 
   if [[ $minutes -eq 15 ]]; then
     vibrate 1;
